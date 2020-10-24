28f493c299f202f4b743567008fca4452ec6bb79
Chris Patrick <atomize101@gmail.com>
2020-10-24 11:29:16 -0400

    Trying to get the API data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+	const [accounts, setAccounts] = useState([]);
+	//const baseURL = 'https://sampleapis.com/fakebank/api/Accounts';
+
+	//useEffect(() => {
+	//	fetch(baseURL).then((response) => response.json().then(setAccounts));
+	//}, []);
+	const baseURL = 'https://sampleapis.com/fakebank/api/Accounts';
+
+	fetch(baseURL)
+		.then((resp) => {
+			return resp.json();
+		})
+
+		.then((data) => console.log(data));
+
 	return (
 		<div className="App">
-			<div>FakeBank</div>
+			{accounts.map((account) => (
+				<button key={account}>{account}</button>
+			))}
 		</div>
 	);
 }
