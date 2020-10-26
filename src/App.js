ea5766e7ba25f3b4364257859d070c3f1c1cbc22
Chris Patrick <atomize101@gmail.com>
2020-10-26 15:01:21 -0400

    Trying to total up the credit and debit transactions

--- src/App.js
+++ src/App.js
@@ -4,23 +4,36 @@ import { getBalance } from './service/api';
 
 function App() {
 	const [accounts, setAccounts] = useState([]);
+	const [total, setTotal] = useState(0);
 
 	useEffect(() => {
 		getBalance().then(setAccounts);
+		setTotal(total);
 	}, []);
 
+	const getTotal = () => {
+		accounts.map((account) => {
+			return account.Debit + account.Credit;
+		});
+	};
 	return (
 		<>
 			<div className="App">
-				{accounts.map((account) => (
-					<div className="balance">
-						<h2 key={account.id}>{account['Transaction-Date']}</h2>
-						<h2 key={account.id[1]}>Description: {account.Description}</h2>
-						<h3 key={account.id[2]}>Category: {account.Category}</h3>
-						<h3 key={account.id[3]}>Debit: {account.Debit}</h3>
-						<h3 key={account.id[4]}>Credit:{account.Credit}</h3>
-					</div>
-				))}
+				<div>
+					<h1>
+						Total Charges:
+						<div className="Total">{total}</div>
+					</h1>
+					{accounts.map((account) => (
+						<div className="balance">
+							<h2 key={account.id}>{account['Transaction-Date']}</h2>
+							<h2 key={account.id[1]}>Description: {account.Description}</h2>
+							<h3 key={account.id[2]}>Category: {account.Category}</h3>
+							<h3 key={account.id[3]}>Debit: {account.Debit}</h3>
+							<h3 key={account.id[4]}>Credit:{account.Credit}</h3>
+						</div>
+					))}
+				</div>
 			</div>
 		</>
 	);
